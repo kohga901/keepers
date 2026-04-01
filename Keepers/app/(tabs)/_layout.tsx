e7b8fa866bc24cd7a5ce2a0a0880e472020b6079
Kai Markley <k.ming.markley@gmail.com>
2026-04-01 00:48:13 -0600

    The app now loads up on swiper and not on anyother page

--- Keepers/app/(tabs)/_layout.tsx
+++ Keepers/app/(tabs)/_layout.tsx
@@ -8,24 +8,29 @@ export default function TabLayout() {
 
   return (
     <Tabs
-    initialRouteName="swiper"
-    screenOptions={{
-      tabBarActiveTintColor: theme.tabActive,
-      tabBarInactiveTintColor: theme.tabInactive,
-      headerStyle: {
-        backgroundColor: theme.headerBg,
-      },
-      headerShadowVisible: false,
-      headerTintColor: theme.headerText,
-      sceneStyle: {
-        backgroundColor: theme.background,
-      },
-      tabBarStyle: {
-        backgroundColor: theme.tabBg,
-        borderTopColor: theme.border,
-      },
-    }}
-  >
+      screenOptions={{
+        tabBarActiveTintColor: theme.tabActive,
+        tabBarInactiveTintColor: theme.tabInactive,
+        headerStyle: {
+          backgroundColor: theme.headerBg,
+        },
+        headerShadowVisible: false,
+        headerTintColor: theme.headerText,
+        sceneStyle: {
+          backgroundColor: theme.background,
+        },
+        tabBarStyle: {
+          backgroundColor: theme.tabBg,
+          borderTopColor: theme.border,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          href: null,
+        }}
+      />
       <Tabs.Screen
         name="likelist"
         options={{
