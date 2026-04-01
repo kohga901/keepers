1da09036008f07f387c6e1fed72f8491267412e4
Kai Markley <k.ming.markley@gmail.com>
2026-03-31 23:22:46 -0600

    Testing Image features on expo

--- Keepers/app/(tabs)/about.tsx
+++ Keepers/app/(tabs)/about.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 import Swiper from "react-native-deck-swiper";
+import { Image } from 'expo-image';
+
 
 const { height } = Dimensions.get("window");
 
 const cards = [
-  { name: "Floral Summer Dress", price: "$49.99" },
-  { name: "Classic Denim Jacket", price: "$89.00" },
-  { name: "Slim Fit Chinos", price: "$34.99" },
-  { name: "Oversized Hoodie", price: "$59.99" },
-  { name: "Striped Button-Up Shirt", price: "$42.00" },
+  { name: "Floral Summer Dress", price: "$49.99", url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTYc1bSnxvvXQRFp_QzSHXai1CfuYWe6Qw_nk-Jsju0roFCzN2NiFGS5_wTVMLSAqkMjlZkF1mufpbcjpxAOB-EV6t_mOsh" },
+  { name: "Classic Denim Jacket", price: "$89.00", url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQHm0M_4d7xaJh6yuZYe2e64mLVAwfPC-6BnSqEf6CwW0TcMLUT0onY6yUD4NHg6idh_fHQz9my1fKuMr1ZjGj1RUYzYnZl" },
+  { name: "Slim Fit Chinos", price: "$34.99", url: "https://www.kuhl.com/_next/image/?url=https%3A%2F%2Fwww.kuhl.com%2Fmedia%2Fuploads%2Fpdp%2F5109%2Fbutcha_overlanding_053.jpg&w=1080&q=100" },
+  { name: "Oversized Hoodie", price: "$59.99", url: "https://ricoma.com/cdn/shop/files/maker-apparel-beige-hoodie-ricoma-embroidery-machines.webp?v=1749140249&width=900" },
+  { name: "Striped Button-Up Shirt", price: "$42.00", url: "https://aurelien-online.com/cdn/shop/files/aurelien_linen_shirt_overhemd_linnen_heren_light_blue_stripe1.jpg?v=1741952773&width=600" },
 ];
 
 export default function App() {
@@ -22,7 +24,13 @@ export default function App() {
           return (
             <View style={styles.card}>
               <View style={styles.imagePlaceholder}>
-                <Text style={styles.imagePlaceholderText}>👕</Text>
+                 <Image
+                    style={styles.image}
+                    source={{ uri: card.url }}
+                    placeholder={{ blurhash }}
+                    contentFit="cover"
+                    transition={1000}
+                  />
               </View>
               <View style={styles.cardInfo}>
                 <Text style={styles.cardName}>{card.name}</Text>
@@ -68,6 +76,9 @@ const overlayLabels = {
     },
   },
 };
+const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -75,6 +86,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#F5FCFF",
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#0553',
   },
   card: {
     borderRadius: 12,
