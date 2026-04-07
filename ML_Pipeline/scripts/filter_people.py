0c6b2ce96a622b9df6fe2e74fe81389acb46f618
Derek Tinoco <slcderek34@gmail.com>
2026-04-06 21:07:29 -0600

    Updated the YOLO model we are using to be newer

--- ML_Pipeline/scripts/filter_people.py
+++ ML_Pipeline/scripts/filter_people.py
@@ -1,3 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")  
+#model = YOLO("yolov8n.pt")  
+# That model is older and uses more CPU, You are welcome to add it back if you need it
+model = YOLO("yolo26n.pt")
+
