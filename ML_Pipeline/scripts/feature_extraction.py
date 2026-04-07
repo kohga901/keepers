5656df5d4715ead9250cb4fe99f9b8a6174a3c2e
Derek Tinoco <slcderek34@gmail.com>
2026-04-07 00:29:55 -0600

    We now parse JSON files, get the item stuffs and visualize with visualize.py

--- ML_Pipeline/scripts/feature_extraction.py
+++ ML_Pipeline/scripts/feature_extraction.py
@@ -1,10 +1,15 @@
 import numpy as np
+import json
 import clip
 import torch
 import os
 from PIL import Image
+import requests # new 
+from io import BytesIO # new
 import logging as log
 import sys
+import numpy as np
+
 
 """
 Clip is OpenAi's clip model from github, gets an embedding for 
@@ -22,8 +27,11 @@ log.basicConfig(level=log.DEBUG)
 BATCH_SIZE = 32     # Number of clothings the CLIP model will be processing at a time.
 
 # init, the folder path is the clothing folder
-folder_path = "../Background_Filtered_Images" # this is where we would get the clothes we scraped...IF WE HAD ANY
-all_files = os.listdir(folder_path)
+# jsonFile_path = ../Scrapers/Google/shoppingListingJSON/google_shopping_njfjte_2026-04-01T02-47-25-227Z
+# for img in jsonFile_path:
+#   image_path.append()
+# folder_path = "../Backgroun#d_Filtered_Images" # this is where we would get the clothes we scraped...IF WE HAD ANY
+# all_files = os.listdir(folder_path)
 
 # turn on metal if u got it
 device = "mps" if torch.backends.mps.is_available() else "cpu"
@@ -62,11 +70,11 @@ def is_image(filename: str) -> bool:
     """
     return filename.lower().endswith(('.jpg', '.jpeg', '.png'))
 
-def to_full_path(filename: str) -> str: 
-    """
-    Builds the full path name to a file.
-    """
-    return os.path.join(folder_path, filename)
+# def to_full_path(filename: str) -> str: 
+#     """
+#     Builds the full path name to a file.
+#     """
+#     return os.path.join(folder_path, filename)
 
 
 # ---------------------------------------------------------------------------------------
@@ -74,30 +82,57 @@ def to_full_path(filename: str) -> str:
 # ---------------------------------------------------------------------------------------
 
 log.debug("Starting script...\n")
-log.debug(f"NUMBER OF ITEMS: {len(all_files)}\n")
+with open("../../Scrapers/Google/shoppingListingJSON/google_shopping_njfjte_2026-04-01T02-47-25-227Z.json", "r") as f:
+    items = json.load(f)
+    
+log.debug(f"NUMBER OF ITEMS: {len(items)}\n")
 
 # filter = trim the non images from all files
-only_images = list(filter(is_image, all_files))
+#only_images = list(filter(is_image, all_files))
 
-log.debug(f"NUMBER OF CLOTHES AFTER FILTER: {len(only_images)}\n")
+#log.debug(f"NUMBER OF CLOTHES AFTER FILTER: {len(only_images)}\n")
 log.debug(f"BATCH SIZE: {BATCH_SIZE}\n")
 
 # Exit if no images in the folder.
-if (len(only_images) == 0):
-    log.debug(f"NO IMAGES TO EMBED. NUMBER OF IMAGES: {len(only_images)}\n")
-    log.debug(f"EXITING...\n")
-    sys.exit()
+# if (len(items) == 0):
+#     log.debug(f"NO IMAGES TO EMBED. NUMBER OF IMAGES: {len(only_images)}\n")
+#     log.debug(f"EXITING...\n")
+#     sys.exit()
 
 # Path of every image.
-image_paths = list(map(to_full_path, only_images))
-
+#image_paths = list(map(to_full_path, only_images))
+valid_items = []
+valid_images = []
 # Compute the number of batches required.
-num_of_batches = (len(image_paths) + (BATCH_SIZE - 1)) // BATCH_SIZE
+#num_of_batches = (len(image_paths) + (BATCH_SIZE - 1)) // BATCH_SIZE
 
 # The vectors that the images have been transformed into. 
 # Each vector is 512 dim.
-embedded_vectors = []
+for item in items:
+    try:
+        response = requests.get(item["item_img"], timeout=5)
+        response.raise_for_status()
+        img = Image.open(BytesIO(response.content)).convert("RGB")
+        valid_images.append(preprocess(img).to(device)) # type: ignore
+        valid_items.append({
+            "item_name": item["item_name"],
+            "item_price": item["item_price"],
+            "item_gender": item["item_gender"],
+            "item_img": item["item_img"],
+            "item_web_listing": item["item_web_listing"]
+        })
+    except Exception as e:
+        log.warning(f"failed on {item.get('item_name', 'unknown')}: {e}")
 
+log.debug(f"Successfully Loaded: {len(valid_images)} images")  
+
+if len(valid_images) == 0:
+    log.debug(f"NO IMAGES TO EMBED. NUMBER OF IMAGES: {len(valid_images)}\n")
+    log.debug(f"EXITING...\n")
+    sys.exit()
+
+embedded_vectors = []
+num_of_batches = (len(valid_images) + BATCH_SIZE - 1) // BATCH_SIZE
 # Get images in batches and convert them to embeddings using the CLIP model.
 for batch_num in range(num_of_batches):
 
@@ -109,36 +144,28 @@ for batch_num in range(num_of_batches):
 
     # Compute the index for getting an image from image_paths.
     start = batch_num * BATCH_SIZE
-    end = min(start + BATCH_SIZE, len(image_paths))
+    end = min(start + BATCH_SIZE, len(valid_images))
 
-    # Go though each image in a batch and feed it into preprocess().
-    for i in range(start, end):
-
-        # Get image from the image_paths list and convert it to a PIL image object.
-        # Shouldn't keep too many image files open, so the file descriptor is closed after being opened. 
-        # (Honestly we won't be opening too many image files at once but just to be safe.)
-        with Image.open(image_paths[i]) as img:
-            img = img.convert("RGB")
-
-        # Put the image through preprocess. 
-        processed_img = preprocess(img).to(device) # type: ignore
-
-        # Put the processed image in the list.
-        batch_of_processed_images.append(processed_img)
+    batch_of_processed_images = valid_images[start:end]
     
-    # Now the list of tensors in "processed_images_batch" must be merged into one big tensor.
-    # Reason for this is because the CLIP model expects one tensor.
     tensor = torch.stack(batch_of_processed_images)
-
-    # Put a batch of processed images through the CLIP model.
+    # We can just process the tensor after stacking
     embeddings = get_embeddings(tensor)
-
-    # Put the embeddings on cpu.
-    # Then convert the tensors to numpy.
-    embeddings_numpy = embeddings.cpu().numpy()
+    embedded_vectors.append(embeddings.cpu().numpy())
     
-    # Put the embeddings in the vector array.
-    embedded_vectors.append(embeddings_numpy)
+    # # Now the list of tensors in "processed_images_batch" must be merged into one big tensor.
+    # # Reason for this is because the CLIP model expects one tensor.
+    # tensor = torch.stack(batch_of_processed_images)
+
+    # # Put a batch of processed images through the CLIP model.
+    # embeddings = get_embeddings(tensor)
+
+    # # Put the embeddings on cpu.
+    # # Then convert the tensors to numpy.
+    # embeddings_numpy = embeddings.cpu().numpy()
+    
+    # # Put the embeddings in the vector array.
+    # embedded_vectors.append(embeddings_numpy)
 
 # Put the extracted features from this batch to the total vectors.
 catalog_embeddings = np.vstack(embedded_vectors)
@@ -146,5 +173,14 @@ catalog_embeddings = np.vstack(embedded_vectors)
 log.debug(f"FINISHED EMBEDDINGS.\n")
 
 np.save("catalog_embeddings.npy", catalog_embeddings) #.npy is like a bunch of numbers loosely formatted to go to ram, bytes go to memory, faster than csv
+with open("metadata.json", "w") as f:
+    json.dump(valid_items, f, indent=2)
 
 log.debug(f"Script finished...\n")
+
+# data = np.load("catalog_embeddings.npy")
+
+# # reduced = TSNE(n_components=2, random_state=42).fit_transform(data)
+# # plt.scatter(reduced[:, 0], reduced[:, 1], alpha=0.5)
+# # plt.title("CLIP Embeddings")
+# # plt.show()
