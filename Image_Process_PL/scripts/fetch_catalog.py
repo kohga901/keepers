c64dac004e507c6e201d597af95b563e2f2d4fc5
kohga901 <khongor0901@gmail.com>
2026-04-14 17:47:34 -0600

    IMplemented skeleton code for fetch_catalog.

--- Image_Process_PL/scripts/fetch_catalog.py
+++ Image_Process_PL/scripts/fetch_catalog.py
@@ -6,11 +6,35 @@ from pathlib import Path
 from dotenv import load_dotenv
 from logger import setup_logging
 
-load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+if (load_dotenv()):
+    DATABASE_URL = os.getenv("DATABASE_URL")
+else:
+    exit(1)
+
 OUTPUT_CSV = "../data/image_urls.csv"
 LOG_PATH = "../logs/fetch_catalog.log"
 
-client = create_client(SUPABASE_URL, SUPABASE_KEY)
-response = client.table("Clothing").select("item_id, item_img").execute()
+def fetch(logger: logging.Logger):
+    try:
+        # Connect to the DB
+        conn = psycopg2.connect(DATABASE_URL)
+
+        # Create a cursor to run queries
+        cursor = conn.cursor()
+
+        # Run a query
+        cursor.execute("SELECT item_id, item_img FROM \"Clothing\";")
+
+        # Fetch all results as a list of tuples
+        rows = cursor.fetchall()
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+        
+        return rows
+
+    except Exception:
+        logger.error(f"Error fetching from db.")
+    
