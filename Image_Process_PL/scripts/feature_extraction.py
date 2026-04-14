4e4ed5d8c4ede1151aa02af91399ba590f8711d8
kohga901 <khongor0901@gmail.com>
2026-04-14 12:50:06 -0600

    Changed feature_extraction

--- Image_Process_PL/scripts/feature_extraction.py
+++ Image_Process_PL/scripts/feature_extraction.py
@@ -12,10 +12,10 @@ feature_extraction.py
 
 Final stage of the pipeline.
 
-- Gets the folder of the filtered clothing
-- Loads OpenAi's clip model from github
-- Gets an embedding for each file in the folder of filtered clothing
-- Saves it to an npy.
+    - Gets the folder of the filtered clothing
+    - Loads OpenAi's clip model from github
+    - Gets an embedding for each file in the folder of filtered clothing
+    - Saves it to an npy.
 """
 
 
