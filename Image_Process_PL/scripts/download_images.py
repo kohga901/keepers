5392c273bd99d2500b3d3fafcead3e80e8e19fb1
kohga901 <khongor0901@gmail.com>
2026-04-14 10:41:03 -0600

    Added new script called download_images.py. This is for downloading images in batches form the csv file's url column.

---
+++ Image_Process_PL/scripts/download_images.py
@@ -0,0 +1,11 @@
+"""
+download_images.py.
+
+Given the path of a csv file:
+    - fetches the all the urls from the item_img column 
+    - download the images
+    - save the images in the downloaded_images folder
+
+Usage:
+    image_downloader.py --batch 64 --limit 4000
+"""
