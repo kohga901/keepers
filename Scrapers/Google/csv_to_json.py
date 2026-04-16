076d880e1b896a93ab70d91ec878f06118aaad36
Gabriel Min <70347715+GBRMIN@users.noreply.github.com>
2026-04-15 23:00:10 -0600

    csv to json mirror of json to csv python file

---
+++ Scrapers/Google/csv_to_json.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from pathlib import Path
+
+src = Path("./shoppingListingCSV")
+dst = Path("./shoppingListingJSON")
+
+dst.mkdir(parents=True, exist_ok=True)
+
+for file in src.iterdir():  # can also do src.glob("*.csv")
+    if file.suffix == ".csv":
+        data = pd.read_csv(file)
+        output = dst / (file.stem + ".json")
+        data.to_json(output, orient="records", indent=2, force_ascii=False)
