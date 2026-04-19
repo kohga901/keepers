32ef90084b3dca3cd1d7cb2d926bc8472761c290
kohga901 <khongor0901@gmail.com>
2026-04-19 02:00:28 -0600

    Uvicorn server startup successful

--- server/services/Startup.py
+++ server/services/Startup.py
@@ -37,59 +37,7 @@ faiss.normalize_L2(_embeddings)
 index = faiss.IndexFlatIP(EMBEDDING_DIM)        # Create empty index
 index.add(_embeddings)                          # Add embeddings
 
-
-# # The embeddings for our catalog will be a 2 dimensional array 
-# # That means the shape is (num_items, 512)
-# # data must be float32, float64 doesn't cut it (we'll crash if we try)
-
-# # There is a normalize method @KO can u figure out what that is? look into normalize_L2 for Faiss?
-# # We CAN normalize in the indexing script after line 62? Or u can do it herev 
-# # norm = 
-# # embedding / norm?
-# # or 
-
-# # np.save at the end tho
-
-
-# # all item vectors should be appeneded to the index
-# # index is like google 512-dimension vectors get fed into FAISS
-# index.add(catalog_embeddings) # type: ignore 
-# # the catalog ids need to be loaded from our database:
-# # gabe this is for you man
-# # Our data base needs these columns:
-# # catalog_ids = ["nike_shirt_001", "Gucci_pants_01" ]
-# # id: A unique string or integer (e.g., nike_shirt_001). This is what you store in your catalog_ids list.
-
-# # image_url: The path to the photo so you can actually show it to the user.
-
-# # product_page_url: So the user can go buy the item.
-
-# # processed_flag: (Crucial!) A true/false column that tells your indexing script, "I've already turned this photo into a vector, don't do it again."
-
-# # we never set processed flag to true but we'll have to do that for every image, probably right before we run the indexingScript.py
-# catalog_ids = []
-
-# # n is the neighbors
-# def get_recommendation_for_every_user(user_profiles: dict, n: int = 20, ) -> dict:
-#     """
-#     T
-#     """
-#     user_ids = list(user_profiles.keys())
-#     profiles = [user_profiles[uid] for uid in user_ids]
-#     #profiles = list(map(user_profiles.get, user_profiles))
-
-#     # STACK all profiles into 2d Matrix
-#     # doing this is like what we did to the clothes in indexingScirp
-#     # This is how we do multiple queries
-#     profiles_matrix = np.array(profiles, dtype=np.float32) #npstack dont work here
-
-#     assert profiles_matrix.ndim == 2
-#     assert profiles_matrix.shape[1] == index.d
-
-#     distancies, indices = index.search(profiles_matrix, k=n)  # type: ignore 
-
-#     recommendations = {}
-#     for i, user_id in enumerate(user_ids):
-#           recommendations[user_id] = [catalog_ids[idx] for idx in indices[i]]
-
-#     return recommendations
+item_id_to_embedding = {
+    item_id: _embeddings[i]
+    for i, item_id in enumerate(_item_ids)
+}
