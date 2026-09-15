16dcae67e00b8f6334aecbd78352f1e1e8540110
Gabriel Min <70347715+GBRMIN@users.noreply.github.com>
2026-09-14 21:53:11 -0600

    make public keys public so users can build app

--- Keepers/utils/supabase.ts
+++ Keepers/utils/supabase.ts
@@ -1,9 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_CONFIG } from '@/public-config'
 
 export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+  process.env.EXPO_PUBLIC_SUPABASE_URL ?? PUBLIC_CONFIG.supabaseUrl,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY ?? PUBLIC_CONFIG.supabaseKey,
   {
     auth: {
       storage: AsyncStorage,
