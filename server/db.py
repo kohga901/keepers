29abd9e4b014e859ec39a89091cad7265ac6ead7
Khongor <u1389938@utah.edu>
2026-09-15 14:45:22 -0600

    Changed checking for .env file since render has it automatically

--- server/db.py
+++ server/db.py
@@ -3,12 +3,9 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 from pathlib import Path
 
-if load_dotenv():
-    SUPABASE_URL = os.getenv("SUPABASE_URL")
-    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-else:
-    exit(1)
+load_dotenv()
 
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-    
